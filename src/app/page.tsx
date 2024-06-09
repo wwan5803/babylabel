d9ba13565f6685463c7a6e8290e35c9ccb43f9f9
Wesley Wang <wesley.wang@onetwo.com.au>
2024-06-10 00:52:18 +1000

    feat: init project

---
+++ src/app/page.tsx
@@ -0,0 +1,37 @@
+import { Check } from "lucide-react";
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-slate-50">
+      <section>
+        <MaxWidthWrapper className="pb-24 pt-10 lg:grid lg:grid-cols-3 sm:pb-32 lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52">
+          <div className="col-span-2 px-6 lg:px-0 lg:pt-4">
+            <div className="relative mx-auto text-cneter lg:text-left flex flex-col items-center lg:items-start">
+              <div className="abolute w-28 left-0-top-20 hidden lg:block">
+                <img src="/snake-1.png" className="w-full" />
+              </div>
+              <h1 className="relative w-fix tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl">
+                Your Image on a{" "}
+                <span className="bg-green-600 px-2 text-white">Custom</span>{" "}
+                Phone Case
+              </h1>
+              <p className="mt-8 text-lg lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap">
+                Capture your favorite memories with your own
+              </p>
+              <ul className="mt-8 space-y-2 text-left font-medium flex flex-col items-center sm:items-start">
+                <div className="space-y-2">
+                  <li className="flex gap-1.5 items-center text-left">
+                    <Check className="w-5 h-5 shrink-0 text-green-600" />
+                    High-quality, durable material
+                  </li>
+                </div>
+              </ul>
+            </div>
+          </div>
+        </MaxWidthWrapper>
+      </section>
+    </div>
+  );
+}
